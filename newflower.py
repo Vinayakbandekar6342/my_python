f848be5469138eea289f765dc4e8a2b75bb46121
Vinayak <vinayakbandekar6342@gmail.com>
2020-10-20 23:01:07 +0530

    file-pract

--- newflower.py
+++ newflower.py
@@ -4,19 +4,31 @@ import myclass
 #print(a)
 a=[]
 
+f=open('flowers.txt','w+',encoding='utf-8')
+
+
 def fun(): #this function is for getting inputs and appending in list
     
 
     for i in range(0,23,1):
 	 
         x = input('Enter name of flower:')
+        f.write('Enter name of flower:%s\n'%x)
         y = input('Enter color of flower:')
+        f.write('Enter color of flower:%s\n'%y)
         z = input('Enter season of flower:')
+        f.write('Enter season of flower:%s\n'%z)
         for i in range (0,1):
             print('\n')
         print('Thank you for the inputs!')
+        f.write('Thank you for the inputs!')
         flo = myclass.flower(x,y,z)
         print([flo.name],"--", [flo.color],"--", [flo.season])
+        p=(flo.name +' is a flower. It has '+flo.color+' color. It blooms in '+flo.season+' season.')
+        print(p)
+        f.write('%s'%p)
+    
+
         for i in range(0,1):
             print('\n')
             
